c9f4acdc1a22cd528290d2e237c35e04658a8f88
Pablolinari <paablolinaari@gmail.com>
2023-11-11 17:42:27 +0100

    .

--- ED/retos/listas/retolistas.cpp
+++ ED/retos/listas/retolistas.cpp
@@ -1,3 +1,4 @@
+#include <ios>
 #include <iterator>
 #include<list>
 #include<iostream>
@@ -15,14 +16,19 @@ list<intervalo>::iterator it , it1;
 for (it = l1.begin(); it != l1.end(); ++it) {
     if ((it->first <= x.first )&& (it->second >= x.second)) {
         pertenece = true;
-        it = it1;
 
-        if(it->first > x.first ){
-            l1.insert(it,intervalo(it->first,x.first));
-        }        
-        else if(it->second < x.second){
+        if(it->first == x.first ){
             l1.insert(it,intervalo(x.second,it->second));
+        }        
+        if(it->second == x.second){
+            
+            l1.insert(it,intervalo(it->first,x.first-1));
         }
+        else{
+        l1.insert(it,intervalo(it->first,x.first-1));
+        l1.insert(it,intervalo(x.second+1,it->second));
+        }
+        l1.erase(it);
         
     }
 }
@@ -47,6 +53,10 @@ int main(){
             l1.push_back(intervalo(n1,n2));
         n++;
     }
+    list<intervalo>::iterator it;
+     for(it=l1.begin();it!=l1.end();++it){
+         cout <<" [" <<it->first <<","<< it->second << "] ,";
+     }
     cout << "intervalo  a buscar : ";
     cout << "Primer numero : \n";
     cin  >> n1;
@@ -59,10 +69,10 @@ int main(){
 
     bool extraido = Extraer(l1, i, l2);
 
-    list<intervalo>::iterator it;
-    for(it=l1.begin();it!=l1.end();++it){
-        cout <<" [" <<it->first <<","<< it->second << "] ,";
-    }
+     for(it=l1.begin();it!=l1.end();++it){
+         cout <<" [" <<it->first <<","<< it->second << "] ,";
+     }
+     cout <<boolalpha << endl << extraido; 
 
     return 0;
 };
