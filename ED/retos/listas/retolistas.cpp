94fb73a08bfea7b3b0cd89656a1fd3bf9c0e341f
Pablolinari <paablolinaari@gmail.com>
2023-11-28 21:19:52 +0100

    .

--- ED/retos/listas/retolistas.cpp
+++ ED/retos/listas/retolistas.cpp
@@ -1,140 +1,136 @@
-#include <ios>
-#include <iostream>
-#include <iterator>
+
 #include <list>
-#include <system_error>
 #include <utility>
-#include <vector>
+#include <iostream>
+
 
 using namespace std;
 typedef pair<int, int> intervalo;
 
-bool Extraer(list<intervalo> &l1, intervalo x, list<intervalo> &l2) {
-  bool pertenece = false, continua = true;
-  list<intervalo>::iterator it, it1;
-  for (it = l1.begin(); it != l1.end() && continua; ++it) {
-    if (it->first <= x.first && it->second >= x.second) {
-      intervalo a(*it);
-      if (a.first < x.first && a.second > x.second) {
-        l1.insert(it, intervalo(a.first, x.first - 1));
-        l1.insert(it, intervalo(x.second + 1, a.second));
-      }
-      if (a.first == x.first) {
-        l1.insert(it, intervalo(x.second + 1, a.second));
-      }
-      else if (a.second == x.second) {
-        l1.insert(it, intervalo(a.first, x.first - 1));
-      }
-      continua = false;
-      pertenece = true;
-      it = l1.erase(it);
-    }
-  }
-  intervalo a;
-  continua = true;
-  it1 = l2.begin();
-  bool sigue1 =true , sigue2 = true;
-  auto it2=it1;
-  for(it = l2.begin(); it != l2.end() && continua ; ++it){
-    if(it->first >= x.first){
-      a.first = x.first;
-      it1 = it;
-    }   
-    if(it->second  <= x.second){
-      a.second = x.second;
-      it2 = it;
-      l2.insert(it,a);
-      continua  = false;
-    }
+// bool Extraer(list<intervalo> &l1, intervalo x, list<intervalo> &l2) {
+//   bool pertenece = false, continua = true;
+//   list<intervalo>::iterator it, it1;
+//   for (it = l1.begin(); it != l1.end() && continua; ++it) {
+//     if (it->first <= x.first && it->second >= x.second) {
+//       intervalo a(*it);
+//       if (a.first < x.first && a.second > x.second) {
+//         l1.insert(it, intervalo(a.first, x.first - 1));
+//         l1.insert(it, intervalo(x.second + 1, a.second));
+//       }
+//       if (a.first == x.first) {
+//         l1.insert(it, intervalo(x.second + 1, a.second));
+//       }
+//       else if (a.second == x.second) {
+//         l1.insert(it, intervalo(a.first, x.first - 1));
+//       }
+//       continua = false;
+//       pertenece = true;
+//       it = l1.erase(it);
+//     }
+//   }
+//   intervalo a;
+//   continua = true;
+//   it1 = l2.begin();
+//   bool sigue1 =true , sigue2 = true;
+//   auto it2=it1;
+//   for(it = l2.begin(); it != l2.end() && continua ; ++it){
+//     if(it->first >= x.first){
+//       a.first = x.first;
+//       it1 = it;
+//     }  
+//     if(it->second  >= x.second){
+//       a.second = it->second;
+//       it2 = it;
+//       continua  = false;
+//       it = l2.erase(it1,++it2);
+//       l2.insert(it,a);
+//     }
     
 
 
-  }
-    l2.erase(it1,it2);
-if(it1 == l2.end())
-    cout << "hola" ;
-  
-  return pertenece;
+//   }
+//   return pertenece;
+// }
+
+bool Extraer(list<intervalo> &l1, intervalo x, list<intervalo> &l2) {
+    bool pertenece = false;
+
+    // Iterador para recorrer l1
+    auto it = l1.begin();
+    while (it != l1.end()) {
+        // Verifica si x está completamente dentro de un intervalo en l1
+        if (it->first <= x.first && it->second >= x.second) {
+            pertenece = true;
+
+            // Si el intervalo es más grande que x, divide el intervalo en l1
+            if (it->first < x.first) {
+                l1.insert(it, make_pair(it->first, x.first - 1));
+                it++;
+            }
+            if (it->second > x.second) {
+                l1.insert(it, make_pair(x.second + 1, it->second));
+                it++;
+            }
+            // Borra el intervalo que contenía a x
+            it = l1.erase(it);
+        } else if (it->first > x.second) {
+            // Si el intervalo actual en l1 está después de x, se termina la búsqueda
+            break;
+        } else {
+            // Avanza al siguiente intervalo en l1
+            it++;
+        }
+    }
+
+    // Iterador para recorrer l2
+    auto it2 = l2.begin();
+    while (it2 != l2.end()) {
+        // Busca el primer intervalo en l2 que comience después de x
+        if (it2->first > x.first) {
+            // Busca el primer intervalo en l2 que termine después de x
+            while (it2 != l2.end() && it2->second < x.second) {
+                it2++;
+            }
+            // Si el intervalo termina después de x, lo actualiza en l2
+            if (it2 != l2.end() && it2->first <= x.second) {
+                it2->first = x.second + 1;
+            }
+            break;
+        }
+        it2++;
+    }
+
+    // Inserta el intervalo x en l2
+    l2.push_back(x);
+
+    return pertenece;
 }
 
+
+void printList(const list<intervalo> &lst) {
+    for (const auto &interval : lst) {
+        cout << "[" << interval.first << ", " << interval.second << "] ";
+    }
+    cout << endl;
+}
+
+
 int main() {
+    list<intervalo> L1 = {{1, 7}, {10, 14}, {18, 20}, {25, 26}};
+    list<intervalo> L2 = {{0, 1}, {14, 16}, {20, 23}};
+    intervalo x1 = {12, 14};
 
-  list<intervalo> l1, l2;
-  intervalo i;
-  i.first = 12;
-  i.second = 14;
-  intervalo l1_1, l1_2, l1_3, l1_4, l2_2, l2_1, l2_3;
-  l1_1.first = 1;
-  l1_1.second = 7;
-  l1_2.first = 10;
-  l1_2.second = 14;
-  l1_3.first = 18;
-  l1_3.second = 20;
-  l1_4.first = 25;
-  l1_4.second = 26;
-  l2_1.first = 0;
-  l2_1.second = 1;
-  l2_2.first = 14;
-  l2_2.second = 16;
-  l2_3.first = 20;
-  l2_3.second = 23;
+    cout << "Antes de Extraer:\nL1: ";
+    printList(L1);
+    cout << "L2: ";
+    printList(L2);
 
-  l1.push_back(l1_1);
-  l1.push_back(l1_2);
-  l1.push_back(l1_3);
-  l1.push_back(l1_4);
-  l2.push_back(l2_1);
-  l2.push_back(l2_2);
-  l2.push_back(l2_3);
+    Extraer(L1, x1, L2);
 
-  list<intervalo>::iterator it;
-  bool extraido = Extraer(l1, i, l2);
+    cout << "\nDespués de Extraer:\nL1: ";
+    printList(L1);
+    cout << "L2: ";
+    printList(L2);
 
-  for (it = l1.begin(); it != l1.end(); ++it) {
-    cout << " [" << it->first << "," << it->second << "] ,";
-  }
-cout  << endl;
-  
-  for (it = l2.begin(); it != l2.end(); ++it) {
-    cout << " [" << it->first << "," << it->second << "] ,";
-  }
-  cout << endl;
-  /////////////////////////////////////////////////////
-
-  i.first = 12;
-  i.second = 20;
-
-  l1_1.first = 1;
-  l1_1.second = 7;
-  l1_2.first = 10;
-  l1_2.second = 22;
-  l1_3.first = 25;
-  l1_3.second = 26;
-
-  l2_1.first = 0;
-  l2_1.second = 1;
-  l2_2.first = 14;
-  l2_2.second = 16;
-  l2_3.first = 20;
-  l2_3.second = 23;
-
-  l2.clear();
-  l1.clear();
-  l1.push_back(l1_1);
-  l1.push_back(l1_2);
-  l1.push_back(l1_3);
-  l2.push_back(l2_1);
-  l2.push_back(l2_2);
-  l2.push_back(l2_3);
-cout << "-------------------------------------------------------------"<<endl;
-  extraido = Extraer(l1, i, l2);
-  for (it = l1.begin(); it != l1.end(); ++it) {
-    cout << " [" << it->first << "," << it->second << "] ,";
-  }
-  cout  << endl;
-  for (it = l2.begin(); it != l2.end(); ++it) {
-    cout << " [" << it->first << "," << it->second << "] ,";
-  }
-  cout << endl;
-  return 0;
-};
+    return 0;
+}
