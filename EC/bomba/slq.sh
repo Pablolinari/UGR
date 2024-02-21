922311802a9986e0f43fe580c1cd2f78c660a849
Pablolinari <paablolinaari@gmail.com>
2024-02-21 10:52:06 +0100

    .

--- EC/bomba/slq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcc $1  -o "mibomba" -no-pie
-
-gdb ./mibomba -ex "layout asm" -ex "layout regs" -ex "b main" -ex "run" -ex "set write on" -ex "set { char } 0x40130d=0xeb" -ex "set { char } 0x401323=0xeb" -ex "set { char } 0x401323=0xeb" -ex "set { char } 0x401323=0xeb" -ex "set { char } 0x401323=0xeb"
-
-
