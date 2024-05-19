6ec3872edc8361103c897c4525c7dbe0294d3f39
Pablolinari <paablolinaari@gmail.com>
2024-05-19 21:24:25 +0200

    .

--- pruebaalg.cpp
+++ pruebaalg.cpp
@@ -1,99 +1,80 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <limits>
-#include <iostream>
 
 using namespace std;
 
-double bound1(const vector<int>& path, const vector<vector<double>>& graph) {
-    double bound = 0.0;
-    for (int i = 0; i < graph.size(); i++) {
-        if (find(path.begin(), path.end(), i) == path.end()) {
-            double minDistance = numeric_limits<double>::max();
-            for (int j = 0; j < graph.size(); j++) {
-                if (i != j && graph[i][j] < minDistance) {
-                    minDistance = graph[i][j];
-                }
+void printSolution(const vector<int> &solution) {
+    for (int city : solution) {
+        cout << city << " ";
+    }
+    cout << endl;
+}
+
+int nextCity(int k, vector<int> &solution, const vector<vector<double>> &graph) {
+    do {
+        solution[k]++;
+        if (solution[k] < graph.size() &&
+            graph[solution[k - 1]][solution[k]] > 0 &&
+            find(solution.begin(), solution.begin() + k, solution[k]) == solution.begin() + k) {
+            if (k == graph.size() - 1 && graph[solution[k]][solution[0]] > 0) {
+                return solution[k];
             }
-            bound += minDistance;
-        }
-    }
-    return bound;
-}
-
-double bound2(const vector<int>& path, const vector<vector<double>>& graph) {
-    double bound = 0.0;
-    for (int i = 0; i < graph.size(); i++) {
-        if (find(path.begin(), path.end(), i) == path.end()) {
-            double minDistance = numeric_limits<double>::max();
-            for (int j : path) {
-                if (graph[i][j] < minDistance) {
-                    minDistance = graph[i][j];
-                }
+            if (k < graph.size() - 1) {
+                return solution[k];
             }
-            bound += minDistance;
         }
-    }
-    return bound;
+    } while (solution[k] < graph.size());
+    solution[k] = -1; // Reset to -1 as 0 is a valid city index
+    return -1;
 }
 
-double bound3(const vector<int>& path, const vector<vector<double>>& graph) {
-    double bound = 0.0;
-    int lastVisited = path.back();
-    for (int i = 0; i < graph.size(); i++) {
-        if (find(path.begin(), path.end(), i) == path.end()) {
-            bound += graph[lastVisited][i];
+void tsp(vector<int> &solution, const vector<vector<double>> &graph, int k, double &best_cost, vector<int> &best_solution, double current_cost) {
+    if (k == graph.size()) {
+        current_cost += graph[solution[k - 1]][solution[0]]; // Add cost to return to the starting city
+        if (current_cost < best_cost) {
+            best_cost = current_cost;
+            best_solution = solution;
         }
-    }
-    return bound;
-}
-
-double bound4(const vector<int>& path, const vector<vector<double>>& graph) {
-    double bound = 0.0;
-    int firstVisited = path.front();
-    for (int i = 0; i < graph.size(); i++) {
-        if (find(path.begin(), path.end(), i) == path.end()) {
-            bound += graph[firstVisited][i];
-        }
-    }
-    return bound;
-}
-
-bool promising(int node, const vector<int>& path, const vector<vector<double>>& graph, double (*bound)(const vector<int>&, const vector<vector<double>>&)) {
-    if (find(path.begin(), path.end(), node) != path.end()) {
-        return false;
-    }
-    double boundValue = bound(path, graph);
-    // Implementa la lógica para decidir si el nodo es prometedor en base a la cota
-    return true;
-}
-
-void tsp(int node, vector<int>& path, const vector<vector<double>>& graph, double (*bound)(const vector<int>&, const vector<vector<double>>&)) {
-    path.push_back(node);
-    if (path.size() == graph.size()) {
-        // Imprime la solución si todos los nodos han sido visitados
-        for (int node : path) {
-            cout << node << " ";
-        }
-        cout << endl;
     } else {
-        for (int i = 0; i < graph.size(); i++) {
-            if (promising(i, path, graph, bound)) {
-                tsp(i, path, graph, bound);
+        do {
+            solution[k] = nextCity(k, solution, graph);
+            if (solution[k] != -1) {
+                tsp(solution, graph, k + 1, best_cost, best_solution, current_cost + graph[solution[k - 1]][solution[k]]);
             }
-        }
+        } while (solution[k] != -1);
     }
-    path.pop_back();
 }
 
 int main() {
-    vector<vector<double>> graph = {
-        // Define tu grafo aquí
-    };
-    vector<int> path;
-    // Elige la función de cota antes de ejecutar el programa
-    double (*bound)(const vector<int>&, const vector<vector<double>>&);
-    bound = bound1; // o bound = bound2; o bound = bound3; o bound = bound4;
-    tsp(0, path, graph, bound);
+    int numCities;
+    cout << "Enter the number of cities: ";
+    cin >> numCities;
+
+    vector<vector<double>> graph(numCities, vector<double>(numCities));
+    cout << "Enter the adjacency matrix (use 0 for no direct path):" << endl;
+    for (int i = 0; i < numCities; ++i) {
+        for (int j = 0; j < numCities; ++j) {
+            cin >> graph[i][j];
+        }
+    }
+
+    vector<int> solution(numCities, -1); // Initialize with -1, to be filled with city indices
+    solution[0] = 0; // Start at city 0
+
+    double best_cost = numeric_limits<double>::max();
+    vector<int> best_solution;
+
+    tsp(solution, graph, 1, best_cost, best_solution, 0);
+
+    if (!best_solution.empty()) {
+        cout << "The best solution found is: ";
+        printSolution(best_solution);
+        cout << "With a cost of: " << best_cost << endl;
+    } else {
+        cout << "No solution found." << endl;
+    }
+
     return 0;
 }
