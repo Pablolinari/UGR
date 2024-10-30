37cb062453c21ad4718bee2fb63d21ecb5f0ca68
Pablolinari <paablolinaari@gmail.com>
2024-10-30 17:41:00 +0100

    termino lectores, falta terminar escritores

--- SCD/p2/lectores.cpp
+++ SCD/p2/lectores.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <iostream>
 #include <iomanip>
 #include <cassert>
@@ -7,57 +8,101 @@
 
 using namespace std ;
 using namespace scd ;
+constexpr int               
+   min_ms    = 5,     // tiempo minimo de espera en sleep_for
+   max_ms    = 20 ; 
 
 class lectorescritor : public HoareMonitor{
 private:
-	bool escrib = true ;
-	int n_lec = 0;
+	bool escrib;
+	int n_lec ;
 
 	CondVar lectura, escritura;
 
 	// constantes ('static' 
-	void ini_lectura();
-	void fin_lectura();
-	void ini_escritura();
-	void fin_escritura();
+public:
+	void ini_lectura(int i);
+	void fin_lectura(int i);
+	void ini_escritura(int i);
+	void fin_escritura(int i);
 	lectorescritor();
 };
 
 lectorescritor::lectorescritor(){
 	n_lec = 0;
-	escrib=true;
+	escrib=false;
+	lectura = newCondVar();
+	escritura = newCondVar();
 }
-void lectorescritor::ini_lectura(){
+void lectorescritor::ini_lectura(int i){
 	if(escrib){
 		lectura.wait();
 	}
+		cout<<"Hebra "<<i<<" empieza a leer"<<endl;
 	n_lec++;
 	lectura.signal();
 }
 
-void lectorescritor::fin_lectura(){
+void lectorescritor::fin_lectura(int i){
 	n_lec--;
 	if(n_lec ==0){
 		escritura.signal();
 	}
+	cout<<"Hebra "<<i<<"  termina de leer"<<endl;
 }
 
-void lectorescritor::ini_escritura(){
+void lectorescritor::ini_escritura(int i){
 	if(n_lec>0 || escrib){
 		escritura.wait();
 	}
 	escrib=true;
+	cout<<"Hebra "<<i<<" empieza a escribir"<<endl;
 }
-void lectorescritor::fin_escritura(){
+void lectorescritor::fin_escritura(int i){
 	escrib=false;
 	if(!lectura.empty()){
 		lectura.signal();
 	}else{
 		escritura.signal();
 	}
+	cout<<"Hebra "<<i<<" termina de escribir"<<endl;
+}
+void lector(MRef<lectorescritor>monitor,int i){
+	while(true){
+		monitor->ini_lectura(i);
+   		this_thread::sleep_for( chrono::milliseconds( aleatorio<min_ms,max_ms>() ));
+		monitor->fin_lectura(i);
+   this_thread::sleep_for( chrono::milliseconds( aleatorio<min_ms,max_ms>() ));
+	}
+
+}void  escritor(MRef<lectorescritor>monitor,int i){
+	while(true){
+		monitor->ini_escritura(i);
+   this_thread::sleep_for( chrono::milliseconds( aleatorio<min_ms,max_ms>() ));
+
+		monitor->fin_escritura(i);
+   this_thread::sleep_for( chrono::milliseconds( aleatorio<min_ms,max_ms>() ));
+	}
+
 }
 int main (int argc, char *argv[]) {
 	int numlec=4,numesc=4;
 	thread lectoras[numlec],escritoras[numesc];
+	
+   MRef<lectorescritor> monitor = Create<lectorescritor>() ;	
+	for(int i = 0; i< numesc; i++){
+		escritoras[i]=thread(escritor,monitor,i);
+	}	
+
+	for(int i = 0; i< numlec; i++){
+		lectoras[i]=thread(lector,monitor,i);
+	}
+	for(int i = 0; i< numlec; i++){
+		lectoras[i].join();
+	}
+	for(int i = 0; i< numesc; i++){
+		escritoras[i].join();
+	}
+
 	return 0;
 }
