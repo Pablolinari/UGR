1ce4b18c276dcbe9ed0a103f939af4f6fb17966d
Pablolinari <paablolinaari@gmail.com>
2024-11-11 11:54:34 +0100

    aniado un poco de fumadores

--- SCD/p2/fumadoresmonitor.cpp
+++ SCD/p2/fumadoresmonitor.cpp
@@ -66,8 +66,8 @@ private:
 	,fumador[num_fumadores];// cola donde esperan los fumadores 
 public:
 	Estanco();
-	int poner_ingrediente(int i);
-	int obtener_ingrediente(int  i);
+	void poner_ingrediente(int i);
+	void obtener_ingrediente(int  i);
 	void esperar_recogida_ingrediente();
 };
 
@@ -76,19 +76,53 @@ Estanco::Estanco(){
 	for(int i=0;i<num_fumadores;i++){
 		fumador[i]=newCondVar();
 	}
+	ingrediente = -1;
 }
-int Estanco::poner_ingrediente(int i){
-	
+void Estanco::poner_ingrediente(int i){
+	cout<<"puesto el ingrediente "<< i <<endl;
+	if(!fumador[i].empty()){
+		fumador[i].signal();
+	}
 }
-int Estanco::obtener_ingrediente(int i){
-   
+void Estanco::obtener_ingrediente(int i){
+   if(ingrediente!=i){
+		fumador[i].wait();
+	}
+	ingrediente =-1;
+	cout << "fumador coge el ingrediente " << i << endl;
+	estanquero.signal();
 }
 void Estanco::esperar_recogida_ingrediente(){
-   
+   if(ingrediente >=0){
+		estanquero.wait();
+	}
 }
-void funcion_hebra_estanquero(){
-
+void funcion_hebra_estanquero(MRef<Estanco>monitor){
+	int ingrediente;
+	while (true) {
+ 		ingrediente = producir_ingrediente();
+		monitor->poner_ingrediente(ingrediente);
+		monitor->esperar_recogida_ingrediente();
+	}
 }
-void funcion_hebra_fumador(int num_fumador){
+void funcion_hebra_fumador(int num_fumador,MRef<Estanco>monitor){
+	while(true){
+		monitor->obtener_ingrediente(num_fumador);
+		fumar(num_fumador);
+	}
+}
 
+int main (int argc, char *argv[]) {
+	MRef<Estanco>monitor = Create<Estanco>();
+	thread fumadores[num_fumadores];
+	thread estanquero = thread(funcion_hebra_estanquero,monitor);
+	for (int i = 0; i < num_fumadores; i++){
+		fumadores[i]=thread(funcion_hebra_fumador,i,monitor);
+	}
+	for (int i = 0; i < num_fumadores; i++){
+		fumadores[i].join();
+	}
+	estanquero.join();
+
+	return 0;
 }
