3d13d999296b1cf0becc7a5abec969d69990726f
Pablolinari <paablolinaari@gmail.com>
2024-12-08 13:44:29 +0100

    primera prueba del codigo , falta compilar y ejecutar

--- SCD/p3/criba_erastotenes.cpp
+++ SCD/p3/criba_erastotenes.cpp
@@ -14,20 +14,71 @@ void procesostart(){
 		num = i;
 		MPI_Send(&num,1, MPI_INT,1,0, MPI_COMM_WORLD);
 	}
+	num=-1;
+	MPI_Send(&num,1, MPI_INT,1,0, MPI_COMM_WORLD);
 
 }
 
 void procesofilter(int id , int next_id){
-	
+	int valor,primo;
+ 	MPI_Status estado;
+	bool continua =true;
+	MPI_Recv(&primo,1, MPI_INT , id-1 ,0, MPI_COMM_WORLD, &estado);
+	MPI_Send(&primo,1, MPI_INT,idprinter,0, MPI_COMM_WORLD);
+
+	while (continua) {
+		MPI_Recv(&valor,1, MPI_INT , id-1 ,0, MPI_COMM_WORLD, &estado);
+		if(valor%primo!=0){
+			MPI_Send(&valor,1, MPI_INT,next_id,0, MPI_COMM_WORLD);
+		}
+		if(valor == -1){
+			continua = false;
+		}
+	}
 }
 
-void procesofilter(int id){
+void procesofinal(int id){
+	int primo;
+	MPI_Status estado;
+	bool continua = true;
+	MPI_Recv(&primo,1, MPI_INT ,id-1,0, MPI_COMM_WORLD, &estado);
+	while (continua) {
+		MPI_Recv(&primo,1, MPI_INT ,id-1,0, MPI_COMM_WORLD, &estado);
+		if(primo == -1){continua=false;}
+	}
 
+}
+
+void procesoprinter(){
+	bool continua = true;
+	MPI_Status estado;
+	int primo ;
+	while (continua) {
+		MPI_Recv(&primo,1, MPI_INT,MPI_ANY_SOURCE,0, MPI_COMM_WORLD, &estado);
+		if(primo == -1){
+			continua = false;
+		}
+	}
 }
 
 int main (int argc, char *argv[]) {
 	MPI_Init(&argc,&argv);
+	int id_propio , num_procesos_actual;
+  MPI_Comm_rank( MPI_COMM_WORLD, &id_propio );
+  MPI_Comm_size( MPI_COMM_WORLD, &num_procesos_actual );
 	
+	if (id_propio == 0) {
+		procesostart();
+	}
+	else if(id_propio == nlimit-1){
+		procesoprinter();
+	}
+	else if (id_propio == nlimit-2) {
+		procesofinal(id_propio);
+	}
+	else{
+		procesofilter(id_propio,id_propio +1);
+	}
 
 
 	MPI_Finalize();
