6421fb5d3a30b8b2676fd8881483c9852aaca570
Pablolinari <paablolinaari@gmail.com>
2026-09-25 20:23:07 +0200

    .

--- UPM/STI/p2/entorno/criptosistema.sh
+++ UPM/STI/p2/entorno/criptosistema.sh
@@ -75,13 +75,15 @@ cd ..
 
 
 cd remitente
-printf "REMITENTE: Encripto el mensaje (RSA)\n"
+printf "REMITENTE: Encripto el mensaje (AES)\n"
 openssl enc -aes-128-cbc -in secreto.txt -out secreto_cifrado.bin -pass file:clave_secreta.hex -pbkdf2
 
 printf "REMITENTE: Genero MAC (DH)\n"
+
 # derivo la clave del MAC pasando el secreto DH por SHA-256
-openssl dgst -sha256 -r dh_clave_remitente.bin | cut -d' ' -f1 > clave_mac_remitente.hex
-openssl dgst -sha256 -mac HMAC -macopt hexkey:"$(cat clave_mac_remitente.hex)" secreto_cifrado.bin > secreto_cifrado.bin.hmac
+# 
+openssl dgst -sha256 -binary -out clave_mac_remitente.bin dh_clave_remitente.bin
+openssl dgst -sha256 -mac HMAC -macopt hexkey:"$(od -An -tx1 -v clave_mac_remitente.bin | tr -d ' \n')" secreto_cifrado.bin > secreto_cifrado.bin.hmac
 
 printf "REMITENTE: Envío el secreto encriptado y documento de integridad al destinatario\n"
 mv secreto_cifrado.bin ../canal/
@@ -95,12 +97,14 @@ cp ../canal/secreto_cifrado.bin.hmac secreto_cifrado.bin.hmac
 printf "DESTINATARIO: recibo secreto encriptado y documento de integridad \n"
 printf "DESTINATARIO: Compruebo la integridad (DH) \n"
 # derivo la clave del MAC pasando el secreto DH por SHA-256
-openssl dgst -sha256 -r dh_clave_destinatario.bin | cut -d' ' -f1 > clave_mac_destinatario.hex
-openssl dgst -sha256 -mac HMAC -macopt hexkey:"$(cat clave_mac_destinatario.hex)" secreto_cifrado.bin > secreto_cifrado_nuevo.bin.hmac
+openssl dgst -sha256 -binary -out clave_mac_destinatario.bin dh_clave_destinatario.bin
+openssl dgst -sha256 -mac HMAC -macopt hexkey:"$(od -An -tx1 -v clave_mac_destinatario.bin | tr -d ' \n')" secreto_cifrado.bin > secreto_cifrado_nuevo.bin.hmac
 if cmp -s secreto_cifrado_nuevo.bin.hmac secreto_cifrado.bin.hmac; then
 	printf "DESTINATARIO: coinciden los HMAC, integridad garantizada\n"
-	printf "DESTINATARIO: desencripto (RSA) \n"
+	printf "DESTINATARIO: desencripto (AES) \n"
 	openssl enc -d -aes-128-cbc -in secreto_cifrado.bin -out secreto_descifrado.txt -pass file:clave_secreta.hex -pbkdf2
 	printf "Secreto desencriptado: %s\n" "$(cat secreto_descifrado.txt)"
+else
+	printf "No conserva integridad , no desencripto"
 fi
 cd ..
